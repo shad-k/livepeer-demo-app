540040bfb107e2cfe891772357b26512ab32a81c
Shadab Khan <shadab.2005@gmail.com>
2021-04-18 21:00:08 +0530

    Add video and connect to livepeer.com api

--- components/AppBody.tsx
+++ components/AppBody.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Hls from "hls.js";
 
 import APIKeyForm from "./APIKeyForm";
 
-import { APP_STATES } from "../types";
+import { APP_STATES } from "../utils/types";
 
 interface Props {
   state: any;
@@ -10,7 +11,62 @@ interface Props {
   createStream: () => void;
 }
 
+const copyTextToClipboard = (text: string) => {
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state == "granted" || result.state == "prompt") {
+      navigator.clipboard.writeText(text);
+    }
+  });
+};
+
 const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
+  const { playbackId, streamIsActive, streamKey } = state;
+  React.useEffect(() => {
+    let hls;
+    if (streamIsActive && playbackId) {
+      const video = document.getElementById("video");
+      const videoSrc = `https://cdn.livepeer.com/hls/${playbackId}/index.m3u8`;
+      if (Hls.isSupported()) {
+        hls = new Hls();
+        // bind them together
+        hls.attachMedia(video as HTMLVideoElement);
+        // MEDIA_ATTACHED event is fired by hls object once MediaSource is ready
+        hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+          console.log("video and hls.js are now bound together !");
+          hls.loadSource(videoSrc);
+          hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+            console.log(
+              "manifest loaded, found " + data.levels.length + " quality level"
+            );
+            (video as HTMLVideoElement).play();
+          });
+        });
+        hls.on(Hls.Events.ERROR, function (event, data) {
+          var errorType = data.type;
+          var errorDetails = data.details;
+          var errorFatal = data.fatal;
+          console.log(errorType, errorDetails, errorFatal);
+          switch (data.details) {
+            case Hls.ErrorDetails.FRAG_LOAD_ERROR:
+              // ....
+              break;
+            default:
+              break;
+          }
+        });
+      } else if (
+        (video as HTMLVideoElement).canPlayType("application/vnd.apple.mpegurl")
+      ) {
+        (video as HTMLVideoElement).src = videoSrc;
+      }
+    }
+
+    return () => {
+      if (hls) {
+        hls.destroy();
+      }
+    };
+  }, [streamIsActive]);
   switch (state.appState) {
     case APP_STATES.API_KEY:
       return <APIKeyForm setApiKey={setApiKey} />;
@@ -30,6 +86,60 @@ const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
         <div className="w-full h-3/5 flex flex-col items-center justify-center">
           <div className="animate-spin w-8 h-8 rounded-full border-2 border-livepeer border-r-0 border-b-0 mb-8"></div>
           Creating stream...
+        </div>
+      );
+    case APP_STATES.WAITING_FOR_VIDEO:
+    case APP_STATES.SHOW_VIDEO:
+      return (
+        <div className="w-full h-3/5 flex flex-col items-center justify-center">
+          <div className="relative bg-black h-3/5 w-full">
+            <video id="video" className="h-full w-full" controls />
+            <div className="bg-white rounded-xl flex items-center justify-center absolute right-2 top-2 p-1 text-xs">
+              <div
+                className={`animate-pulse ${
+                  streamIsActive ? "bg-green-700" : "bg-yellow-600"
+                } h-2 w-2 mr-2 rounded-full`}
+              ></div>
+              {streamIsActive ? "Live" : "Paused"}
+            </div>
+          </div>
+
+          <div className="w-11/12 border border-dashed p-2 m-4 flex flex-col text-sm">
+            <div className="flex items-center justify-between mt-2 break-all	">
+              <span>
+                Playback URL:
+                <br />
+                https://cdn.livepeer.com/hls/{playbackId}/index.m3u8
+              </span>
+              <button
+                onClick={() =>
+                  copyTextToClipboard(
+                    `https://cdn.livepeer.com/hls/${playbackId}/index.m3u8`
+                  )
+                }
+                className="border ml-1 p-1 rounded text-sm break-normal"
+              >
+                Copy
+              </button>
+            </div>
+            <div className="flex items-center justify-between mt-2 break-all	">
+              <span>
+                Injest URL:
+                <br />
+                rtmp://rtmp.livepeer.com/live/{streamKey}
+              </span>
+              <button
+                onClick={() =>
+                  copyTextToClipboard(
+                    `rtmp://rtmp.livepeer.com/live/${streamKey}`
+                  )
+                }
+                className="border ml-1 p-1 rounded text-sm break-normal"
+              >
+                Copy
+              </button>
+            </div>
+          </div>
         </div>
       );
     default:
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-import { APP_STATES } from "../types";
+import { APP_STATES } from "../utils/types";
 import AppBody from "../components/AppBody";
+import { createStream, getStreamStatus } from "../utils/apiFactory";
 
 const INITIAL_STATE = {
   appState: APP_STATES.API_KEY,
   apiKey: null,
+  streamId: null,
   playbackId: null,
   streamKey: null,
   streamIsActive: false,
@@ -15,26 +17,32 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "SUBMIT_API_KEY":
       return {
+        ...state,
         appState: APP_STATES.CREATE_BUTTON,
         apiKey: action.payload.apiKey,
       };
     case "CREATE_CLICKED":
       return {
+        ...state,
         appState: APP_STATES.CREATING_STREAM,
       };
     case "STREAM_CREATED":
       return {
+        ...state,
         appState: APP_STATES.WAITING_FOR_VIDEO,
+        streamId: action.payload.streamId,
         playbackId: action.payload.playbackId,
         streamKey: action.payload.streamKey,
       };
     case "VIDEO_STARTED":
       return {
+        ...state,
         appState: APP_STATES.SHOW_VIDEO,
         streamIsActive: true,
       };
     case "VIDEO_STOPPED":
       return {
+        ...state,
         appState: APP_STATES.WAITING_FOR_VIDEO,
         streamIsActive: false,
       };
@@ -49,6 +57,50 @@ const reducer = (state, action) => {
 
 export default function App() {
   const [state, dispatch] = React.useReducer(reducer, INITIAL_STATE);
+
+  React.useEffect(() => {
+    if (state.appState === APP_STATES.CREATING_STREAM) {
+      (async function () {
+        const streamCreateResponse = await createStream(state.apiKey);
+
+        if (streamCreateResponse.data) {
+          const {
+            id: streamId,
+            playbackId,
+            streamKey,
+          } = streamCreateResponse.data;
+          dispatch({
+            type: "STREAM_CREATED",
+            payload: {
+              streamId,
+              playbackId,
+              streamKey,
+            },
+          });
+        }
+      })();
+    }
+
+    let interval;
+    if (state.streamId) {
+      interval = setInterval(async () => {
+        const streamStatusResponse = await getStreamStatus(
+          state.apiKey,
+          state.streamId
+        );
+        if (streamStatusResponse.data) {
+          const { isActive } = streamStatusResponse.data;
+          dispatch({
+            type: isActive ? "VIDEO_STARTED" : "VIDEO_STOPPED",
+          });
+        }
+      }, 5000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [state.appState]);
 
   return (
     <main className="container pb-12 h-screen">
@@ -65,7 +117,7 @@ export default function App() {
 
         <button
           className="border p-2 h-1/2 rounded border-livepeer hover:bg-livepeer hover:text-white"
-          onClick={() => dispatch({ type: "RESET" })}
+          onClick={() => dispatch({ type: "RESET_DEMO_CLICKED" })}
         >
           Reset Demo
         </button>
