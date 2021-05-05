65e18f42a5580fa912ff8019fd3ed76ac6c4485e
Shadab Khan <shadab.2005@gmail.com>
2021-05-05 19:34:27 +0530

    Use purge option to remove unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./**/*.html", "./**/*.tsx"],
   theme: {
     colors: {
       ...colors,
